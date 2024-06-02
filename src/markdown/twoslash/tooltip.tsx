fc8fa60b75a9f85c3b9896a5d35f8ac2343b8354
ZHANGYU <hey@zhangyu.dev>
2024-06-02 22:26:06 +0800

    update twoslash style

--- src/markdown/twoslash/tooltip.tsx
+++ src/markdown/twoslash/tooltip.tsx
@@ -10,14 +10,17 @@ export const TwoslashTooltip = props => {
       placement='bottom left'
       offset={4}
       arrowBoundaryOffset={8}
-      className='shiki-twoslash max-w-[80vw] origin-top-left rounded border bg-surface shadow-lg md:max-w-[90ch]'
+      shouldFlip={false}
+      className='shiki-twoslash max-w-[80vw] origin-top-left md:max-w-[90ch]'
     >
-      {!noArrow && (
-        <OverlayArrow>
-          <span className='relative block border-[6px] border-transparent border-b-surface before:absolute before:-top-[1.5px] before:left-0 before:-z-10 before:-translate-x-1/2 before:-translate-y-1/2 before:border-[7px] before:border-transparent before:border-b-border before:content-[""]' />
-        </OverlayArrow>
-      )}
-      {children}
+      <div className='rounded border bg-surface shadow-lg'>
+        {!noArrow && (
+          <OverlayArrow>
+            <span className='relative block border-[6px] border-transparent before:absolute before:left-0 before:top-0 before:-translate-x-1/2 before:-translate-y-1/2 before:border-[7px] before:border-transparent before:border-b-border before:content-[""] after:absolute after:left-0 after:top-px after:z-10  after:-translate-x-1/2 after:-translate-y-1/2 after:border-[6px] after:border-transparent after:border-b-surface after:content-[""]' />
+          </OverlayArrow>
+        )}
+        {children}
+      </div>
     </Tooltip>
   )
 }
