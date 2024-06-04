5e3637fcf962bc469e5ad3b073997f8beba9166e
ZHANGYU <hey@zhangyu.dev>
2024-06-04 13:10:44 +0800

    update twoslash tooltip

--- src/markdown/twoslash/tooltip.tsx
+++ src/markdown/twoslash/tooltip.tsx
@@ -1,42 +1,35 @@
 'use client'
-import { useId } from 'react'
 
 import { OverlayArrow } from 'react-aria-components'
 
 import { Tooltip } from '@/components/ui'
-import { useIsServer } from '@/hooks/useIsServer'
 
-export const TwoslashTooltip = props => {
-  const { noArrow, children } = props
-  const id = useId()
-  const isServer = useIsServer()
+interface TwoslashTooltipProps {
+  noArrow?: boolean
+  children: React.ReactNode
+  portalContainer?: HTMLElement
+}
+
+export const TwoslashTooltip = (props: TwoslashTooltipProps) => {
+  const { noArrow, children, portalContainer } = props
 
   return (
-    <>
-      <span id={id} hidden />
-      <Tooltip
-        placement='bottom left'
-        offset={4}
-        arrowBoundaryOffset={8}
-        shouldFlip={false}
-        className='shiki-twoslash max-w-[80vw] origin-top-left md:max-w-[90ch]'
-        UNSTABLE_portalContainer={
-          isServer
-            ? undefined
-            : // eslint-disable-next-line @typescript-eslint/no-non-null-asserted-optional-chain
-              document.getElementById(id)?.closest('.shiki.twoslash')!
-                .parentElement!
-        }
-      >
-        <div className='not-prose rounded border bg-surface shadow-lg'>
-          {!noArrow && (
-            <OverlayArrow>
-              <span className='relative block border-[6px] border-transparent before:absolute before:left-0 before:top-0 before:-translate-x-1/2 before:-translate-y-1/2 before:border-[7px] before:border-transparent before:border-b-border before:content-[""] after:absolute after:left-0 after:top-px after:z-10  after:-translate-x-1/2 after:-translate-y-1/2 after:border-[6px] after:border-transparent after:border-b-surface after:content-[""]' />
-            </OverlayArrow>
-          )}
-          {children}
-        </div>
-      </Tooltip>
-    </>
+    <Tooltip
+      placement='bottom left'
+      offset={4}
+      arrowBoundaryOffset={8}
+      shouldFlip={false}
+      className='shiki-twoslash max-w-[80vw] origin-top-left md:max-w-[90ch]'
+      UNSTABLE_portalContainer={portalContainer}
+    >
+      <div className='not-prose rounded border bg-surface shadow-lg'>
+        {!noArrow && (
+          <OverlayArrow>
+            <span className='relative block border-[6px] border-transparent before:absolute before:left-0 before:top-0 before:-translate-x-1/2 before:-translate-y-1/2 before:border-[7px] before:border-transparent before:border-b-border before:content-[""] after:absolute after:left-0 after:top-px after:z-10  after:-translate-x-1/2 after:-translate-y-1/2 after:border-[6px] after:border-transparent after:border-b-surface after:content-[""]' />
+          </OverlayArrow>
+        )}
+        {children}
+      </div>
+    </Tooltip>
   )
 }
--- src/markdown/twoslash/triger.tsx
+++ src/markdown/twoslash/triger.tsx
@@ -1,14 +1,32 @@
 'use client'
 
+import { cloneElement, useRef } from 'react'
+
 import { TooltipTrigger, Button } from 'react-aria-components'
 
-export const TwoslashTrigger = props => {
+import { useMounted } from '@/hooks/useMounted'
+
+interface TooltipTriggerProps {
+  children: React.ReactElement[]
+  className?: string
+  open: boolean
+}
+
+export const TwoslashTrigger = (props: TooltipTriggerProps) => {
   const { children, className, open } = props
   const [span, tooltip] = children
+
+  const mounted = useMounted()
+  const ref = useRef<HTMLElement>(null)
+
   return (
     <TooltipTrigger delay={0} isOpen={open}>
-      <Button className={className} {...span.props} />
-      {tooltip}
+      <Button ref={ref} className={className} {...span.props} />
+      {mounted &&
+        cloneElement(tooltip, {
+          portalContainer:
+            ref.current!.closest('.shiki.twoslash')!.parentElement,
+        })}
     </TooltipTrigger>
   )
 }
